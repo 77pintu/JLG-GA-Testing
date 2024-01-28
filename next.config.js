6d5e88bfedc0fe19cb811f6a205082adcaa46084
mahalingappa.b <mahalingappa.b@harbingergroup.com>
2024-01-28 17:52:07 +0530

    url updated

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'cpcontents.adobe.com',
+        hostname: 'cpcontents.adobe.com%25',
       },
       {
         protocol: 'https',
