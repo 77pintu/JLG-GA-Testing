653a50fe3c336c50032075579c51fda78a101f98
mahalingappa.b <mahalingappa.b@harbingergroup.com>
2024-01-28 13:09:00 +0530

    new deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+       {
+        protocol: 'https',
+        hostname: 'media.go1static.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cpcontents.adobe.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.lynda.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ pages/[id]/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function CourseDetails() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <h4>JLG Course details</h4>
+        </div>
+    )
+}
