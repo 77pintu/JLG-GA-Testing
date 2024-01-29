43407defd8da5e229fabad3dc2b0db8bfef14f3a
mahalingappa.b <mahalingappa.b@harbingergroup.com>
2024-01-29 17:04:09 +0530

    small changes done

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function CourseDetails() {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <h4>JLG Course detail</h4>
+            <h4>JLG Course details.</h4>
         </div>
     )
 }
