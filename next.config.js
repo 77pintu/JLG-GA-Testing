ee1b0e55dbcae86db517a486d0f84fa4225391d7
mahalingappa.b <mahalingappa.b@harbingergroup.com>
2024-01-29 18:23:17 +0530

    next.config file updated

--- next.config.js
+++ next.config.js
@@ -1,10 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  poweredByHeader: false,
   reactStrictMode: true,
+  output: 'standalone',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
+    dangerouslyAllowSVG: true,
     remotePatterns: [
-       {
+      {
         protocol: 'https',
         hostname: 'media.go1static.com',
       },
@@ -15,10 +33,14 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'cdn.lynda.com',
-      }
+      },
+      {
+        protocol: 'https',
+        hostname: 'stjlgprodstaticwebapp.blob.core.windows.net',
+      },
     ],
   },
-  output: "standalone",
+  i18n,
 }
 
 module.exports = nextConfig
